51e930e638318a20a12eb68415766ac4696d1819
Pedro Raposo <pedrohunter290@gmail.com>
2021-03-24 11:04:14 -0300

    Criação da estrutura padrão e view de teste

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const app = require("./config/server.js");
-const PORT = process.env.PORT || 100;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado na porta ${PORT}`);
