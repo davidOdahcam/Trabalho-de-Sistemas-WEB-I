218aa79de2b570e3fbd53af6034b35102080e9fa
Victor de Oliveira Martins Azevedo <victor2007azevedo@hotmail.com>
2021-03-31 10:56:27 -0300

    mais algumas alterações no script

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -1,63 +1,87 @@
-function CPF(cpf){
-    if(typeof(cpf == "number")) cpf = cpf.toString();
-    if(cpf.length > 11) {
+function CPF(cpf) {
+    if (typeof (cpf == "number")) cpf = cpf.toString();
+    if (cpf.length > 11) {
         cpf = cpf.replace("-", "");
         cpf = cpf.split(".").join("");
     }
-    if(cpf.length > 11 || cpf.length < 11) return false;
+    if (cpf.length > 11 || cpf.length < 11) return false;
     let resultDigit = 0;
-    for(let i = 10; i > 1; i--) {
-        resultDigit += cpf[10-i] * i;
-    } 
+    for (let i = 10; i > 1; i--) {
+        resultDigit += cpf[10 - i] * i;
+    }
 
     resultDigit = resultDigit % 11 == 0 || resultDigit % 11 == 1 ? 0 : 11 - resultDigit % 11;
 
-    if(resultDigit != cpf[9]) return false;
+    if (resultDigit != cpf[9]) return false;
 
     resultDigit = 0;
 
-    for(let i = 11; i > 1; i--) {
-        resultDigit += cpf[11-i] * i;
+    for (let i = 11; i > 1; i--) {
+        resultDigit += cpf[11 - i] * i;
     }
 
     resultDigit = resultDigit % 11 == 0 || resultDigit % 11 == 1 ? 0 : 11 - resultDigit % 11;
 
-    if(resultDigit != cpf[10]) return false;
+    if (resultDigit != cpf[10]) return false;
 
     return true;
 
 }
-function Password(pass){
-    if(pass.length<5 || pass.length>255) return false;
+function Password(pass) {
+    if (pass.length < 5 || pass.length > 255) return false;
     return true;
 }
-function confirmPassword(pass,cpass){
-    if(pass === cpass) return true;
+function confirmPassword(pass, cpass) {
+    if (pass === cpass) return true;
     return false;
 }
-function birthdate(date){
+function checkBirthdate(birthdate) {
+    if(birthdate == "") return false;
+
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth()+1;
+    let day = date.getDate();
+    let b_year = parseInt(birthdate.slice(0,4));
+    let b_month = parseInt(birthdate.slice(5,7));
+    let b_day = parseInt(birthdate.slice(8,10));
+    console.log(b_day, day)
+    console.log(b_month, month)
+    console.log(b_year - year)
+    if(b_year > year || year - b_year > 110 ){
+        console.log("ano troll")
+    }
+    if(b_year == year && b_month > month){
+        console.log("mês troll")
+    }
+    if(b_year == year && b_month == month && b_day > day){
+        console.log("Dia burro")
+    }
+
+    
+
 
 }
 
-function displayMessage(name,form,type,msg){
-    if(document.getElementById(`small-${name}`)){
+function displayMessage(name, form, type, msg) {
+    if (document.getElementById(`small-${name}`)) {
         form.removeChild(form.lastChild);
     }
     let small = document.createElement("small");
-    if(type == true){
-        small.innerText =`${name} válido`;
+    if (type == true) {
+        small.innerText = `${name} ${msg}`;
         small.className = "text-success";
         small.id = `small-${name}`;
         form.appendChild(small);
-    }else{
-        small.innerText =`${name} inválido`;
+    } else {
+        small.innerText = `${name} ${msg}`;
         small.className = "text-danger";
         small.id = `small-${name}`;
         form.appendChild(small);
     }
 }
 
-function validate(){
+function validate() {
     let forms = document.getElementsByClassName("form-group")
     let name = document.getElementById("name").value;
     let cpf = document.getElementById("cpf").value;
@@ -67,24 +91,38 @@ function validate(){
     let birthdate = document.getElementById("birthdate").value;
     let error = 0;
 
-    if(CPF(cpf) === false){
-        displayMessage("CPF",forms[1],false,"CPF inválido")
+    if (CPF(cpf) === false) {
+        displayMessage("CPF", forms[1], false, "inválido");
         error = 1;
-    }else{
-        displayMessage("CPF",forms[1],true)
-    }if(Password(password) === false){
-        console.log("senha invalida");
-        error = 1;
-    }if(confirmPassword(password,confirm_password) === false){
-        console.log("senha incompativel");
-        error = 1;
+    } else {
+        displayMessage("CPF", forms[1], true, "válido");
     }
-    
-    if(error == 1){
+
+    if (Password(password) === false) {
+        displayMessage("Senha", forms[3], false, "inválida");
+        error = 1;
+    } else {
+        displayMessage("Senha", forms[3], true, "válida");
+    }
+
+    if (confirmPassword(password, confirm_password) === false) {
+        displayMessage("Senhas", forms[4], false, "diferentes");
+        error = 1;
+    } else {
+        displayMessage("Senhas", forms[4], true, "compatíveis");
+    }
+
+    if (checkBirthdate(birthdate) === false) {
+
+    } else {
+
+    }
+
+    if (error == 1) {
         return false;
-    }else{
+    } else {
         document.getElementById("form").submit();
-    }   
+    }
 
 }
 
