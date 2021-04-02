93518be669ca0296e3f5a913910dda6b4ea33c66
Pedro Raposo <pedrohunter290@gmail.com>
2021-04-02 10:53:03 -0300

    Adicionando cookieParser, bodyParser e expressSession

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const consign = require("consign");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const expressSession = require("express-session");
+const cookieParser = require("cookie-parser");
 
 let app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-
 app.set("view engine", "ejs");
 app.set("views", "./app/views");
 
@@ -21,6 +21,8 @@ app.set("views", "./app/views");
 // app.use(auth); // HABILITAR QUANDO COOKIES FOREM IMPLEMENTADOS
 
 app.use(express.static("./app/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 consign()
     .include("app/routes")
