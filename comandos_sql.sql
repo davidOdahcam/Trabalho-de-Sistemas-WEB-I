61a119b0d03ce658e7fbc9a25b1a382344ba12d8
David dos Santos Machado <david.desenvolvedor@gmail.com>
2021-04-28 17:00:13 -0300

    Finalizado 102%

---
+++ comandos_sql.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE `web1`;
+
+USE `web1`;
+
+CREATE TABLE `users` (
+  `id` INT(10) AUTO_INCREMENT PRIMARY KEY,
+  `name` VARCHAR(100) NOT NULL,
+  `cpf` VARCHAR(14) NOT NULL,
+  `email` VARCHAR(255) UNIQUE NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `phone` VARCHAR(15) NOT NULL,
+  `birthdate` DATE
+);
